88d0d18e788beea00365b7764d7527cb9de0b63c
Yasuhiro Usutani <y_usutani@mac.com>
2020-06-28 10:41:28 +0900

    Change timezone

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module TryDatetimeSelectReiwa
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
